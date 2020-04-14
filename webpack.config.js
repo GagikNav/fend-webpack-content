6d01d7d2040587891751d9ef1271f5ef8c5b0e84
GagikNav <60577739+GagikNav@users.noreply.github.com>
2020-04-14 17:21:29 +0200

    ex1

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
 const webpack = require('webpack');
+const json = require('json-loader!./file.json');
 
 module.exports = {
 	entry: './src/client/index.js',
 
 	module: {
-		rules: [{ test: '/.js$/', exclude: /node_modules/, loader: 'babel-loader' }],
+		loaders: [
+			{
+				test: /\.json$/,
+				loader: 'json-loader',
+			},
+		],
+		rules: [
+			{ test: '/.js$/', exclude: /node_modules/, loader: 'babel-loader' },
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+					},
+				],
+			},
+		],
 	},
 };
