7acbc4a7126a5e8e17ed7f1ff346cd38433fb599
GagikNav <60577739+GagikNav@users.noreply.github.com>
2020-04-14 18:40:29 +0200

    mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 module.exports = {
+   mode: 'production',
    entry: './src/client/index.js',
    plugins: [
       new HtmlWebPackPlugin({
-         template:
-            './src/client/views/index.html',
+         template: './src/client/views/index.html',
          filename: './index.html',
       }),
    ],
